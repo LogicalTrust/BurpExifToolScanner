cd49567b1356f8d5b47d8be0ea48f6d6e0faffed
mmm <michal.dardas@gmail.com>
2017-11-16 17:50:03 +0100

    background thread

--- src/net/logicaltrust/ExifToolEditorTab.java
+++ src/net/logicaltrust/ExifToolEditorTab.java
@@ -3,6 +3,8 @@ package net.logicaltrust;
 import java.awt.Component;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import burp.IMessageEditorTab;
 import burp.ITextEditor;
@@ -12,6 +14,7 @@ public class ExifToolEditorTab implements IMessageEditorTab {
 	private final ITextEditor textEditor;
 	private final ExifToolProcess exiftoolProcess;
 	private final SimpleLogger logger;
+	private static final ExecutorService POOL = Executors.newCachedThreadPool();
 
 	public ExifToolEditorTab(ITextEditor textEditor, ExifToolProcess exiftoolProcess, SimpleLogger logger) {
 		this.textEditor = textEditor;
@@ -43,19 +46,22 @@ public class ExifToolEditorTab implements IMessageEditorTab {
 	@Override
 	public void setMessage(byte[] content, boolean isRequest) {
 		if (!isRequest && content.length > 0) {
-			try {
-				List<String> metadata = exiftoolProcess.readMetadata(content);
-				if (!metadata.isEmpty()) {
-					String metadataText = String.join("\n", metadata);
-					textEditor.setText(metadataText.getBytes(StandardCharsets.UTF_8));
-					logger.debug("Metadata read from exiftool [IMessageEditorTab] ");
-				} else {
-					logger.debug("No data read from exiftool [IMessageEditorTab]");
+			POOL.execute(() -> {
+				try {
+					List<String> metadata = exiftoolProcess.readMetadata(content);
+					if (!metadata.isEmpty()) {
+						String metadataText = String.join("\n", metadata);
+						textEditor.setText(metadataText.getBytes(StandardCharsets.UTF_8));
+						logger.debug("Metadata read from exiftool [IMessageEditorTab] ");
+					} else {
+						logger.debug("No data read from exiftool [IMessageEditorTab]");
+					}
+				} catch (Exception e) {
+					e.printStackTrace(logger.getStderr());
 				}
-			} catch (Exception e) {
-				e.printStackTrace(logger.getStderr());
-			}
+			});
 		}
+
 	}
 
 	@Override
